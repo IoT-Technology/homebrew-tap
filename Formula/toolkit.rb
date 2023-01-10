9262031bfc94706cf750551165e6fa2d28e56d55
sanshengshui <lovewsic@gmail.com>
2023-01-10 13:52:37 +0800

    upgrade toolkit 0.6.0 to 0.6.1

--- Formula/toolkit.rb
+++ Formula/toolkit.rb
@@ -2,12 +2,12 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Toolkit < Formula
-  desc "Toolkit is a IoT protocol client CLI for IoT developer and learners. CoAP and MQTT protocol are currently supported. It supports colors, autocompletion, internationalization(chinese, english and german…) and multi-platforms"
+  desc "Toolkit is a IoT protocol client CLI for IoT developer and learners. CoAP、MQTT and NB-IoT protocol are currently supported. It supports colors, autocompletion, internationalization(chinese, english and german…) and multi-platforms"
   homepage "https://github.com/IoT-Technology/IoT-Toolkit"
-  url "https://github.com/IoT-Technology/IoT-Toolkit/releases/download/0.6.0/toolkit-0.6.0-osx-x86_64.zip"
+  url "https://github.com/IoT-Technology/IoT-Toolkit/releases/download/0.6.1/toolkit-0.6.1-osx-x86_64.zip"
   sha256 "a65840afa2314a38003b71ce032ffa088d354fc85b5fa293b88c6a9525f74bf2"
   license "Apache-2.0"
-  version "0.6.0"
+  version "0.6.1"
 
   # depends_on "cmake" => :build
 
@@ -21,6 +21,6 @@ class Toolkit < Formula
 
   test do
     output = shell_output("#{bin}/toolkit --version")
-    assert_match "toolkit/0.6.0", output
+    assert_match "toolkit/0.6.1", output
   end
 end
