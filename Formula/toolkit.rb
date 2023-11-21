acfbb663a57b4993a5940c1390bd8ecf2c2122ad
sanshengshui <lovewsic@gmail.com>
2023-11-21 14:07:01 +0800

    upgrade toolkit 0.7.0 to 0.7.1

--- Formula/toolkit.rb
+++ Formula/toolkit.rb
@@ -4,10 +4,10 @@
 class Toolkit < Formula
   desc "Toolkit is a IoT protocol client CLI for IoT developer and learners. CoAP、MQTT and NB-IoT protocol are currently supported. It supports colors, autocompletion, internationalization(chinese, english and german…) and multi-platforms"
   homepage "https://github.com/IoT-Technology/IoT-Toolkit"
-  url "https://github.com/IoT-Technology/IoT-Toolkit/releases/download/0.7.0/toolkit-0.7.0-osx-x86_64.zip"
-  sha256 "a530366eeb364139b5c07894ff7bf2b153052804babb86f6031d2862685234e0"
+  url "https://github.com/IoT-Technology/IoT-Toolkit/releases/download/0.7.1/toolkit-0.7.1-osx-x86_64.zip"
+  sha256 "3ac3852e157ef982e94fb451e1794bdf188d76476ca25ca2fef5ccc37a3cc11c"
   license "Apache-2.0"
-  version "0.7.0"
+  version "0.7.1"
 
   # depends_on "cmake" => :build
 
@@ -21,6 +21,6 @@ class Toolkit < Formula
 
   test do
     output = shell_output("#{bin}/toolkit --version")
-    assert_match "toolkit/v0.7.0", output
+    assert_match "toolkit/v0.7.1", output
   end
 end
